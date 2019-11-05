edba93c6e0664c04fc3189b8d3925f62be14a1cb
Wesley K <wesley.ksk@gmail.com>
2019-11-05 12:49:20 +0800

    First commit

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,51 @@
+package com.example.demo.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String username;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "address_id", referencedColumnName = "id")
+	private Address address;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	
+	
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO address(id, street, city) VALUES(1, 'some street', 'some city');
+INSERT INTO user(id, username, address_id) VALUES(1, 'Wesley K.', 1);
